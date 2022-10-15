f1c6f1571fb1c7e7fa8dfbfec39bacf042475816
valentina1901 <vgaviria415@misena.edu.co>
2022-10-15 13:16:59 -0500

    evidencia callback_asyncawai

--- evidencias/asyncaw1.js
+++ evidencias/asyncaw1.js
@@ -1,1 +1,18 @@
-//https://api.spacexdata.com/v5/launches/latest
+
+const axios = require('axios')
+
+let config = {
+    method:'get',
+    url: 'https://lookup.binlist.net/45717360'
+
+}
+const f = async()=>{
+    try {
+        //creando la promesa 
+       let response=await axios(config)
+       console.log(response.data.scheme)
+    } catch (error) {
+        console.error(error)
+    }
+}
+f()
--- evidencias/callback1.js
+++ evidencias/callback1.js
@@ -1,1 +1,41 @@
-//https://techy-api.vercel.app/api/json
+//https://techy-api.vercel.app/api/json  
+
+const request = require('request')
+
+
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+let endpoint='https://api.disneyapi.dev/characters'
+
+const exito=(response)=>{
+    //se convierte a json
+    const tipos= JSON.parse(response).data
+    tipos.forEach((disney)=>{
+        console.log(`Nombre: ${disney.name}`)  
+        console.log(`TvShows: ${disney.tvShows}`)  
+        console.log('............................................') 
+    })
+}
+
+const fallo=(status)=>{
+  console.log(status)
+}
+
+const get_data=(endpoint,exito, fallo)=>{
+    //2.se crea el objeto de conexion
+    let http= new XMLHttpRequest()
+    //3. abrir conexion
+    http.open('get', endpoint)
+    //4.enviar la solicitud (request) al API 
+    http.send()
+    //5. hacer el tratamiento de response sea de error o de falla 
+    http.onload= ()=>{
+        if(http.status === 200){
+            exito(http.responseText)
+        }else{
+            fallo(http.status)
+        } 
+    }
+}
+//invocar la funcion get_data
+get_data(endpoint, exito, fallo)
