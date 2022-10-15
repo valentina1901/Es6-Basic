94d006c53bd9fca67f99842ac759b93c5c274451
valentina1901 <vgaviria415@misena.edu.co>
2022-10-15 18:44:32 -0500

    url

---
+++ url.js
@@ -0,0 +1,10 @@
+//negocio: https://lookup.binlist.net/45717360
+//rickandmarty: https://rickandmortyapi.com/api/character
+//anime: https://animechan.vercel.app/api/random
+//disney: https://api.disneyapi.dev/characters
+//pokemon: https://pokeapi.co/api/v2/type
+//paises: https://restcountries.com/v3.1/all
+//stranger: https://strangerthings-quotes.vercel.app/api/quotes/5
+//bob burger: https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=150
+//d&d: https://www.dnd5eapi.co/api/ability-scores/cha
+
