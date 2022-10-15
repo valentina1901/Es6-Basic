774fdf5dfcc62b15d625e0e101770239f8914d5c
valentina1901 <vgaviria415@misena.edu.co>
2022-10-15 17:20:38 -0500

    Evidencias finalizadas

--- evidencias/promises1.js
+++ evidencias/promises1.js
@@ -1,1 +1,26 @@
-//https://reqres.in/api/users?page=2
+//axios depedencia 
+//traer axios
+const axios = require('axios')
+
+let config = {
+    method:'get',
+    url: 'https://www.dnd5eapi.co/api/ability-scores/cha'
+
+}
+//creando la promesa 
+axios(config)
+.then((response)=>{
+    //return va a la data linea 16
+    return response.data.skills
+})
+.then((data)=>{
+data.forEach(element=>{
+    console.log(`Tipo: ${element.name}`)
+    console.log(`Url: ${element.url}`)  
+    console.log('.............----------------') 
+})
+})
+.catch((error)=>{
+    console.error(error)
+    console.log('Siga intentando') 
+})
